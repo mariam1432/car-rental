e144783702b6008d8a3843148fa72a4cab81d3d0
fatimaazehraa <iamfatima08@gmail.com>
2025-06-19 00:07:29 -0700

    MyFirstChange

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import {Layout} from'../components'
+
 const Contactus = () => {
   return (
-    <div>Contactus</div>
+   <Layout> <div>Contactus</div></Layout>
   )
 }
 
